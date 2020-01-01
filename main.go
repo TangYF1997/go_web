30e73914526d3e941c533640569c43cab888888c
liuzhenxiong <2447808092@qq.com>
2020-01-01 17:38:58 +0800

    登录

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func test(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("index.html"))
+	t.Execute(w, "")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+	fmt.Println("username中的内容为：", username, password)
+	if username == "123" && password == "456" {
+		t := template.Must(template.ParseFiles("index.html"))
+		// 执行
+		t.Execute(w, "账号密码正确！")
+	} else {
+		t := template.Must(template.ParseFiles("index.html"))
+		// 执行
+		t.Execute(w, "账号密码错误！")
+	}
+}
 
 func main() {
-	fmt.Println("Hello world！")
-	
+	http.HandleFunc("/test", test)
+	http.HandleFunc("/login", login)
+
+	http.ListenAndServe(":8080", nil)
 }
